d0f8da89327df52c4902ce59565e91dc1f8bb85c
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-05-26 23:12:42 +0100

    Update Versus.R
    Adición de ejemplos

--- Versus.R
+++ Versus.R
@@ -1,17 +1,18 @@
-Versus<-function(df=df1,var1="Y",var2="CDP",
-                 minmax=NULL,plot=F,disc=T){
 
+Versus<-function(df,var1=names(df)[1],var2=names(df)[2],
+                 minmax=NULL,plot=TRUE,disc=TRUE){
+  
   #Si dsc == T entonces v<-table()... en lugar de numeros
   #Si cualquier variable tiene un solo valor pierde el entido y brea
-
-  ifelse(length(unique(df[,var1]))>20,break(),"")
+  
+  ifelse(length(unique(df[,var1]))>20,quit(),"")
   #si es un factor volverla n?mero
   #ifelse(class(df[,var1]) | length(unique(df[,var1]))>50,,)
   #minmax=c(0,1)
   a<-df[,var2]
   ifelse(is.null(minmax),c(minimo<-min(a),maximo<-max(a))
          ,c(minimo<-minmax[1],maximo<-minmax[2]))
-
+  
   if(length(unique(df[,var2]))>500){
     v<-as.data.frame(table(cut(df[,var2],seq(minimo, maximo, length.out=100),include.lowest = T),
                            df[,var1]))
@@ -19,7 +20,7 @@ Versus<-function(df=df1,var1="Y",var2="CDP",
   else{
     v<-as.data.frame(table(df[,var2],df[,var1]))
   }
-
+  
   ifelse(length(unique(v$Var1))<=length(unique(v$Var2)),
          c(xt<-unique(v$Var1),var<-"Var1",vir<-"Var2"),
          c(xt<-unique(v$Var2),var<-"Var2",vir<-"Var1"))
@@ -41,8 +42,8 @@ Versus<-function(df=df1,var1="Y",var2="CDP",
   }
   porcent<-cbind(porcent,proporciones)
   names(porcent)[1] <- paste("Intervalo",var2,"")
-
-
+  
+  
   if(plot==T){
     for (k in (ncol(porcent)-length(list)+1):ncol(porcent)) {
       win.graph()
@@ -52,3 +53,5 @@ Versus<-function(df=df1,var1="Y",var2="CDP",
   }
   return(porcent)
 }
+
+#data(iris)
