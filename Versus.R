15b7d7017f6ca021f06d07bf427c980eddd9ea44
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-05-27 19:26:24 +0100

    Update Versus.R
    Cambio Plot y Propósito funcional

--- Versus.R
+++ Versus.R
@@ -1,12 +1,14 @@
+library(ggplot2)
 
 Versus<-function(df,var1=names(df)[1],var2=names(df)[2],
                  minmax=NULL,plot=TRUE,disc=TRUE){
+  #Una de las vaiables debe ser cont. y la otra disc.
   
   #Si dsc == T entonces v<-table()... en lugar de numeros
   #Si cualquier variable tiene un solo valor pierde el entido y brea
   
   ifelse(length(unique(df[,var1]))>20,break(),"")
-  #si es un factor volverla n?mero
+  #si es un factor volverla número
   #ifelse(class(df[,var1]) | length(unique(df[,var1]))>50,,)
   #minmax=c(0,1)
   a<-df[,var2]
@@ -16,10 +18,10 @@ Versus<-function(df,var1=names(df)[1],var2=names(df)[2],
   if(length(unique(df[,var2]))>500){
     v<-as.data.frame(table(cut(df[,var2],seq(minimo, maximo, length.out=100),include.lowest = T),
                            df[,var1]))
-  }
-  else{
+  }else{
     v<-as.data.frame(table(df[,var2],df[,var1]))
   }
+  
   
   ifelse(length(unique(v$Var1))<=length(unique(v$Var2)),
          c(xt<-unique(v$Var1),var<-"Var1",vir<-"Var2"),
@@ -32,7 +34,7 @@ Versus<-function(df,var1=names(df)[1],var2=names(df)[2],
   #by = la variable con m?s valores
   J<-list[[1]]
   for (i in 1:(length(list)-1)){
-    J<-left_join(J,list[[i+1]],by = vir)
+    J<-dplyr::left_join(J,list[[i+1]],by = vir)
   }
   sumas<-rowSums(J[,startsWith(names(J),"Freq")])
   porcent<-J
@@ -45,13 +47,16 @@ Versus<-function(df,var1=names(df)[1],var2=names(df)[2],
   
   
   if(plot==T){
-    for (k in (ncol(porcent)-length(list)+1):ncol(porcent)) {
-      win.graph()
-      plot(porcent[,k],main = paste(var1,"vs",var2),ylab = names(porcent)[k],xlab = var2,col=floor(runif(1,10,30)))
-      lines(porcent[,k])
-    }
+    
+    names(df)[1]<-"var1"
+    names(df)[2]<-"var2"
+    plot_1<-ggplot(df,aes(var2))+geom_density(aes(fill=var1),alpha=.7)
+    plot_1
+    
   }
   return(porcent)
 }
 
 #data(iris)
+
+
