d376874687a764c9d879b2c51bad9719be61c9b3
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-06-11 19:21:38 +0100

    Update RBIND.R
    Definición de todos los casos

--- RBIND.R
+++ RBIND.R
@@ -1,27 +1,28 @@
 RBIND<-function(df1,df2){
   nd1<-ncol(df1);nd2<-ncol(df2)
-  if(names(df1)==names(df2)){
+  if(nd1==nd2 &&
+     all(names(df1)==names(df2))){
     return(rbind(df1,df2))
-  }
-  else{
-    mxm<-c(nd1,nd2)==max(c(nd1,nd2))
+  }else{
+    mxm<-c(nd1,nd2)!=max(c(nd1,nd2))
     dmx<-list(df1,df2)[mxm][[1]]
     dmn<-list(df1,df2)[!mxm][[1]]
     min_ex<-names(dmn)[!names(dmn) %in% names(dmx)]
     max_ex<-names(dmx)[!names(dmx) %in% names(dmn)]
     if(length(min_ex)==0){
-      dmn<-cbind(dmn,as_tibble(matrix(NA,nrow(dmn),c(nd1,nd2)[mxm]-c(nd1,nd2)[!mxm],
+      dmn<-cbind(dmn,tibble::as_tibble(matrix(NA,nrow(dmn),c(nd1,nd2)[mxm]-c(nd1,nd2)[!mxm],
                                       dimnames = list(1:nrow(dmn),max_ex))))
-    }
-    else{
-
-      dmn<-cbind(dmn,as_tibble(matrix(NA,nrow(dmn),length(max_ex),
+    }else{#Csos en donde ambos df tengan otras columnas
+      dmn<-cbind(dmn,tibble::as_tibble(matrix(NA,nrow(dmn),length(max_ex),
                                       dimnames = list(1:nrow(dmn),max_ex))))
-
-      dmx<-cbind(dmx,as_tibble(matrix(NA,nrow(dmx),length(min_ex),
+      
+      dmx<-cbind(dmx,tibble::as_tibble(matrix(NA,nrow(dmx),length(min_ex),
                                       dimnames = list(1:nrow(dmx),min_ex))))
-
+      
     }
-    return(rbind(dmx,dmn))
+    if(nd1<nd2)#regresarlo en el orden correcto
+      return(rbind(dmx,dmn))
+    else
+      return(rbind(dmn,dmx))
   }
 }
