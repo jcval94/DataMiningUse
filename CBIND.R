c905365aadc089094197bea34a0ef0e7e8e4acd1
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-06-12 16:12:00 +0100

    Update CBIND.R
    Ejemplos y funcionamiento completo

--- CBIND.R
+++ CBIND.R
@@ -1,29 +1,35 @@
+
 CBIND<-function(df1,df2){
   is_df<-purrr::map_lgl(list(df1,df2),is.data.frame)
   if(any(is_df)){#hay al menos uno que no es df
-    df1<-tibble::as_tibble(df1);df2<-tibble::as_tibble(df2)
+    df1<-tibble::as_tibble(df1)
+    df2<-tibble::as_tibble(df2)
   }
   nd1<-nrow(df1);nd2<-nrow(df2)
   if(nd1==nd2){
     return(cbind(df1,df2))
   }
   else{
-    mxm<-c(nd1,nd2)==max(c(nd1,nd2))
+    mxm<-c(nd1,nd2)!=max(c(nd1,nd2))
     dmx<-list(df1,df2)[mxm][[1]]
     dmn<-list(df1,df2)[!mxm][[1]]
     
     df_rw<-c(nd1,nd2)[mxm]-c(nd1,nd2)[!mxm]
-    dmn<-rbind(dmn,tibble::as_tibble(matrix(NA,df_rw,ncol(dmn),
-                                    dimnames = list(rep(NA,df_rw),names(dmn)))))
-
+    dmx<-rbind(dmx,tibble::as_tibble(matrix(NA,-df_rw,ncol(dmx),
+                               dimnames = list(rep(NA,-df_rw),names(dmx)))))
+    #a partir de aquí ambos df's tienen las mismas columnas
+  if(is_df[1]){
+    return(cbind(dmn,dmx))
+  }else{
     return(cbind(dmx,dmn))
+  }
   }
 }
 
 data(iris)
 d_1<-iris[,5]
 
-df1<-iris
 df2<-d_1
+df1<-rbind(iris,iris)
 
-CBIND(iris,d_1)
+CBIND(df1,df2)
