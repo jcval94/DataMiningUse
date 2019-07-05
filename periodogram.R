03ae11e572092a13b082659d812678ac6a2d3aac
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-07-05 23:44:15 +0100

    Update periodogram.R
    Quit useless code

--- periodogram.R
+++ periodogram.R
@@ -1,47 +1,30 @@
-periodicidad<-function(df,place=10,fast=TRUE,pruebas=1000){
-  #df es una serie de tiempo ordenada
-  #Hacer el proceso máx 3 veces
+periodicidad<-function(ts,place=10){
+  
   ddT<-data.frame(freq=c(),spec=c(),orden=c())
-  #Desde el .3 hasta el final
-  ords<-floor(length(df)*.3):length(df)
-  #La propuesta es usar intervalos aleatorios distintos
-  #De lo contrario se tardará mucho
-  repp<-length(ords)
-  if(repp>pruebas & fast){
-    #Se reducirá a 1000
-    ords<-sort(sample(x = ords,size = pruebas))
+  ords<-floor(length(ts)*.7):length(ts)
+  
+  for(lu in ords){
+    #Se identifica estacionalidad semanal si la hay 
+    p<-TSA::periodogram(ts[1:lu],plot=F)
+    
+    #spectrum(ts.anual,log="no")
+    dds<- data.frame(freq=1/p$freq, spec=p$spec,orden=1:length(p$spec))
+    dds<-head(dds[order(-dds$spec),],place)
+    
+    ddT<-rbind(ddT,dds)
   }
-  for(i in 1:2){
-    for(lu in ords){
-      if(i==1){
-        #Se identifica estacionalidad si la hay
-        p<-TSA::periodogram(df[1:lu],plot=F)
-      }
-      else if (i==2){
-        p<-TSA::periodogram(df[(lu+1-min(ords)):length(df)],plot=F)
-      }
-      #spectrum(ts.anual,log="no")
-      dds<- data.frame(freq=1/p$freq, spec=p$spec,orden=1:length(p$spec))
-      dds<-head(dds[order(-dds$spec),],place)
-      #si es la primera la omitimos porque es evidente la estacionalidad del total
-      #Aqu? descartamos aquellas frecuencias que no alcanan el 6.5% de la freq m?xima
-      ddT<-rbind(ddT,dds)
-      #Regresa los datos agrupados conforme a las primeras 5 frecuencias
-    }
-  }
+  
   
   ddT<-ddT[order(-ddT$spec),]
   Maxi<-max(ddT$spec)
-  #Una periodicidad menor a 3 obs es sospechosa para un periodo
-  ddT<-ddT[ddT$orden>2,]
-  dd_Top<-head(ddT,30)
-  #Opciones distintas
-  dd_Top$Freq_Orden<-paste0(dd_Top$freq,"_",dd_Top$orden)
-  dd_Top<-suppressWarnings(reshape2::dcast(dd_Top,Freq_Orden~.,max,value.var="spec"))
-  dd_Top$.<-dd_Top$./Maxi
-  dd_Top<-dd_Top[order(-dd_Top$.),]
+  ddT<-head(ddT[ddT$orden>2,],15)
   
-  return(list(unique(as.numeric(do.call("rbind",strsplit(dd_Top$Freq_Orden,"_"))[,1])),dd_Top))
+  ddT$Freq_Orden<-paste0(ddT$freq,"_",ddT$orden)
+  ddT<-suppressWarnings(reshape2::dcast(ddT,Freq_Orden~.,max,value.var="spec"))
+  ddT$.<-ddT$./Maxi
+  ddT<-ddT[order(-ddT$.),]
+  
+  return(list(unique(as.numeric(do.call("rbind",strsplit(ddT$Freq_Orden,"_"))[,1])),ddT))
 }
 
 
