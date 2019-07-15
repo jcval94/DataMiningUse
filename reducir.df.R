622ebf8a35227f48d09b00b04430245bc069f767
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-07-15 16:03:43 +0100

    Create reducir.df.R
    Eliminar columnas con grandes porcentajes de NA's

---
+++ reducir.df.R
@@ -0,0 +1,7 @@
+reducir.df<-function(df,max_na){
+  if(max_na>1 | max_na<0){return(NULL)}
+  df[,purrr::map_lgl(df,~length(is.na(.x))/
+                       length(.x)>max_na)]
+}
+
+reducir(iris,.5)
