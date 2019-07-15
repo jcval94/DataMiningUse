f5f2b91d7e99e4e873c182724c1b837cd5fca371
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-07-15 16:17:34 +0100

    Update and rename reducir.df.R to reducir.df.col.R
    rename

--- reducir.df.col.R
+++ reducir.df.col.R
@@ -1,4 +1,4 @@
-reducir.df<-function(df,max_na){
+reducir.df.col<-function(df,max_na){
   if(max_na>1 | max_na<0){return(NULL)}
   df[,purrr::map_lgl(df,~length(is.na(.x))/
                        length(.x)>max_na)]
