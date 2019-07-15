7bdd4ae762793b11b50ac886ce71122f44353a42
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-07-15 20:55:13 +0100

    Update reducir.df.col.R

--- reducir.df.col.R
+++ reducir.df.col.R
@@ -1,7 +1,7 @@
 reducir.df.col<-function(df,max_na){
   if(max_na>1 | max_na<0){return(NULL)}
-  df[,purrr::map_lgl(df,~length(is.na(.x))/
-                       length(.x)>max_na)]
+  df[,purrr::map_lgl(df,~length(.x[is.na(.x)])/
+                       length(.x)<max_na)]
 }
 
 reducir(iris,.5)
